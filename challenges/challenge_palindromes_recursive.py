7b6908f09d8c788b4cebda229ec1b7e5c50ba509
Josimar <josimarsaldanha@gmail.com>
2023-06-04 12:26:08 -0300

    feat: challenge palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if len(word) == 0:
+        return False
+    if low_index >= high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(
+            word,
+            low_index + 1,
+            high_index - 1,
+        )
     raise NotImplementedError
